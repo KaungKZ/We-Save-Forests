f67d0b49294ae6b59d3c240975c17e52b63d6d2a
Kaung Khant Zaw <kaungkz93@gmail.com>
2020-04-12 03:33:45 +0630

    finished overall css styles

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,14 @@ import "../css/style.scss";
 
 const nav_btn = document.querySelector(".menu-for-phone");
 const header_menu = document.querySelector("header .menu");
+const header = document.querySelector("header");
+const mobile_list = document.querySelector("header .menu .list");
 
 nav_btn.addEventListener("click", function () {
   this.classList.toggle("mfp-active");
-  header_menu.classList.toggle("menu-active");
+  header_menu.classList.toggle("mfp-header_menu-active");
+  header.classList.toggle("mfp-header-active");
+  mobile_list.classList.toggle("mfp-list-active");
 });
 // import "./css/style.scss";
 
